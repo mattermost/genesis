e93e4790c6c1a46d32258326282c09000d52e28c
Stylianos Rigas <stylianosrigas@gmail.com>
2021-03-08 13:55:26 +0000

    MM-32522 Fix typo

--- model/account.go
+++ model/account.go
@@ -9,7 +9,7 @@ import (
 	"io"
 )
 
-// Account represents a Kubernetes account.
+// Account represents an AWS account.
 type Account struct {
 	ID                  string
 	State               string
